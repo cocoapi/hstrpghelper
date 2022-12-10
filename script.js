8f7c958760f4bb342ba7d7761bffbf10008bb0f2
cocoapi <eksrns22tp@gmail.com>
2022-12-11 02:47:38 +0900

    선택지 편집기 작업

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+window.scenarioData = {}
+
 function loadScript() {
   const input = loadScripts
   if(input.files.length !== 0){
@@ -27,18 +29,77 @@ function onReaderLoad(event){
 }
 
 function addSelection(){
+  const selEditor = makeSelecionEditor('', '', '');
+  document.querySelector('.selection-add-wrapper').append(selEditor);
+}
+
+function selectionSave(target){
+  console.log(target.parentElement)
+  const selEditor = target.parentElement;
+  const type = selEditor.querySelector('select[name=event-type]')
+  const text = selEditor.querySelector('input.selection-text')
+  const sceneNum = selEditor.querySelector('input.scene-num-input')
+  if(sceneNum.value == '0'){
+    alert('씬 번호가 필요합니다.');
+    sceneNum.style.borderColor = '#ff0000';
+    sceneNum.focus();
+    return;
+  }
+  const editBtn = document.createElement('div')
+  editBtn.classList.add('btn')
+  editBtn.innerText = '수정'
+  const removeBtn = document.createElement('div');
+  removeBtn.classList.add('btn');
+  removeBtn.innerText = '제거';
+  removeBtn.onclick = () => {selectionRemove(removeBtn, true)};
+  selEditor.innerHTML = `
+    <div class="selection-text" style="width: 300px;"> ${text.value} </div>
+    <div class="selection-type"> (${type.value}) </div>
+    <div> 타겟 씬: # </div>
+    <div class="scene-num" style="width: 20px;"> ${sceneNum.value} </div>
+  `
+  selEditor.append(editBtn, removeBtn);
+}
+
+function selectionRemove(target, check = false){
+  if(check && !confirm('정말 삭제하시겠습니까?')){
+    return;  
+  }
+  target.parentElement.remove();
+}
+
+function makeSelecionEditor(setType = '텍스트', setText = '', setSceneNum = ''){
+  if(typeof setType !== 'string' || typeof setText !== 'string' || typeof setSceneNum != 'string'){
+    return;
+  }
   const selEditor = document.createElement('div');
-  selEditor.classList.add('row')
+  selEditor.classList.add('row', 'selection-item');
   const selType = document.createElement('select');
   selType.innerHTML = `
-  <option value="text" selected>텍스트</option>
+  <option value="텍스트"${setType === '텍스트' ? ' selected': ''}>텍스트</option>
   `
+  selType.name = 'event-type';
+  selType.ariaLabel = 'event-type';
   const text = document.createElement('input');
-  text.type = text;
+  text.type = 'text';
   text.placeholder = "텍스트 입력"
+  text.style.width = '300px'
+  text.classList.add('selection-text');
+  text.value = setText;
   const sceneInput = document.createElement('input');
   sceneInput.type = 'number';
   sceneInput.placeholder = "타겟 씬 번호";
-  selEditor.append(selType, text, sceneInput);
-  document.querySelector('.selection-add-wrapper').append(selEditor);
+  sceneInput.value = setSceneNum;
+  sceneInput.classList.add('scene-num-input')
+
+  const saveBtn = document.createElement('div');
+  saveBtn.innerText = '저장'
+  saveBtn.classList.add('btn', 'save');
+  saveBtn.onclick = () => {selectionSave(saveBtn)};
+  const removeBtn = document.createElement('div');
+  removeBtn.classList.add('btn', 'remove');
+  removeBtn.innerText = '제거';
+  removeBtn.onclick = () => {selectionRemove(removeBtn)};
+  selEditor.append(selType, text, sceneInput, saveBtn, removeBtn);
+  return selEditor;
 }
